8d1bcfeafc095ba996760c15801f7ccfc47bce24
DUSHIMIMANA Benjamin <dushimimanabenjamin64@gmail.com>
2026-06-09 09:51:40 +0200

    change category to Category

--- app/Models/category.php
+++ app/Models/category.php
@@ -6,7 +6,7 @@ use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
     use HasFactory;
